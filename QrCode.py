e716ca780014ca8f626e6a6116c1c8763b6397af
EloyQBC <138987017+XEBNPRJP@users.noreply.github.com>
2025-01-19 14:25:38 -0600

    Update QrCode.py

--- QrCode.py
+++ QrCode.py
@@ -13,6 +13,7 @@ def generate_qr_code(data, file_name):
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
 
+print("Welcome to Eloy's QR Code Generator,")
 text= input("Write the website you want to generate a QR code for: ")
 file_name="qr_code.png"
 generate_qr_code(text, file_name)
